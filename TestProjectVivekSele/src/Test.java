be88d109e99fbf06c5c225384e915ae4ff5c52e9
yperaboina <40818100+yperaboina@users.noreply.github.com>
2018-07-04 12:52:39 +0530

    added static block

--- TestProjectVivekSele/src/Test.java
+++ TestProjectVivekSele/src/Test.java
@@ -1,9 +1,14 @@
 
 public class Test {
+	
+	static{
+		System.out.println("load first static block");
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("first Test.java file created");
+      
 	}
 
 }
